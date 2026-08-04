f30d7062469f550ae944b825bea61d60c9a68462
Harshan suthar <harshitsuthar0806@gmail.com>
2026-08-04 20:56:59 +0530

    primeCart

--- Ecommerce-ts/src/pages/checkout/CheckoutHeader.jsx
+++ Ecommerce-ts/src/pages/checkout/CheckoutHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import CheckoutLockIcon from "../../assets/images/icons/checkout-lock-icon.png";
-import Logo from "../../assets/images/logo.png";
+// import Logo from "../../assets/images/logo.png";
+import Logo from "../../assets/images/primeCart.png";
 import MobileLogo from "../../assets/images/mobile-logo.png";
 import "./Checkout-header.css";
 
